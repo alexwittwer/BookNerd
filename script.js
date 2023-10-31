e1a0f824b7f7ab26e7e25bb92e13645a248358ac
alexwittwer <wittweral@gmail.com>
2023-10-31 16:13:55 -0400

    add fetch and google books api

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//google books api
+import fetch from "fetch";
+fetch(
+  "https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyD8XTgw6x-BfOfL6LiRJGqlo4TBuiarnNM"
+)
+  .then((response) => response.json())
+  .then((result) => {
+    this.setState({ books: result.items });
+  });
+
+async function fetchBooks(search) {
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyD8XTgw6x-BfOfL6LiRJGqlo4TBuiarnNM`
+  );
+  const result = await response.json();
+  return result.items;
+}
+
 // Constructor function for library
 
 function Book(title, author, pages, read) {
