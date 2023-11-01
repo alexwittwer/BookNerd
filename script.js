f42a0891902a3fdea79312cc4d145ccaab7e1b56
alexwittwer <wittweral@gmail.com>
2023-10-31 21:25:07 -0400

    remove node packages

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-//google books api
-import fetch from "fetch";
-
-async function fetchBooks(search) {
-  const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyD8XTgw6x-BfOfL6LiRJGqlo4TBuiarnNM`
-  );
-  const result = await response.json();
-  return result.items;
-}
-
-const books = await fetchBooks("search");
-const searchBar = document.querySelector("#search-bar");
-
-document.createElement("div").innerHTML(books);
-
 // Constructor function for library
 
 function Book(title, author, pages, read) {
